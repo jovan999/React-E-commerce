3bf5e16ef512898a059b99db7247fac59f7e1dac
Jovan <smiljko99@gmail.com>
2021-01-18 20:36:26 +0100

    adding sing-in form

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from './pages/shop/shop.component.jsx'
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
+import SingInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SingInAndSignUp} />
       </Switch>
     </div>
   );
